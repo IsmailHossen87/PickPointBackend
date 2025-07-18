cf66b0f6655270d1549f7661e8bdb9f5c155a210
IsmailHossen87 <ismailhosen8757@gmail.com>
2025-07-18 22:22:30 +0600

    google

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { router } from "./app/routes"
 import { globalErrorHandler } from "./app/middleware/GlobalErrorHandlare"
 import { notFound } from "./app/middleware/notFound"
 import cookieParser from "cookie-parser"
+import passport from "passport"
+import expressSession from "express-session"
 
 
 
@@ -11,6 +13,14 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(expressSession({
+    secret :"Your secret",
+    resave:false,
+    saveUninitialized:false
+}))
 
 // Router 
 app.use("/api/v1",router)
--- src/app/modules/auth/auth.router.ts
+++ src/app/modules/auth/auth.router.ts
@@ -1,9 +1,16 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { AuthControler } from "./auth.controler";
+import passport from "passport";
 
 const router = Router()
 router.post("/login",AuthControler.credentialLogin)
 router.post("/logout",AuthControler.logout)
 router.post("/refresh-token",AuthControler.getNewAccessToken)
+
+// google diye authentication
+router.get("/google",async(req:Request,res:Response,next:NextFunction)=>{
+    passport.authenticate("google",{scope:["profile","emaiil"]})(req,res)
+})
+router.get("/google/callback",AuthControler.googleCallbackController)
 
 export const AuthRoutes = router;
